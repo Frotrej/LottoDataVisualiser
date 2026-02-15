25322966f43c25ddace06ada5c83e3058fc1a6ff
Frotrej <f.faszczowy@gmail.com>
2026-02-15 20:16:17 +0100

    Created testing repo

---
+++ Lotto.Tests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using LottoDataVisualiser.Services;
+
+namespace Lotto.Tests
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+			var Lotto = new HttpClientService();
+
+			var result = Lotto.GetResults(2, 3);
+
+			Assert.NotNull(result);
+			Assert.Equal(6, result);
+
+		}
+
+		[Theory]
+		[InlineData(2, 3)]
+		[InlineData(4, 5)]
+		public void Test2(int a, int b)
+		{
+			var Lotto = new HttpClientService();
+			
+			var result = Lotto.GetResults(a, b);
+
+			Assert.NotNull(result);
+			Assert.Equal(a*b, result);
+
+		}
+
+	}
+}
