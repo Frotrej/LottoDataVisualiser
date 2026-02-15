190f5f5ab275debafa4536c5bada2785520e41cd
Frotrej <f.faszczowy@gmail.com>
2026-02-15 12:28:56 +0100

    Dodaj pliki projektów.

---
+++ LottoDataVisualiser/Models/LottoGameResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LottoDataVisualiser.Models
+{
+	internal class LottoGameResult
+	{
+		public string GameName { get; set; }
+		public DateTime DrawDate { get; set; }
+		public int[] WinningNumbers { get; set; }
+		public int BonusNumber { get; set; }
+		public int JackpotAmount { get; set; }
+		
+		public LottoGameResult(string gameName, DateTime drawDate, int[] winningNumbers, int bonusNumber, int jackpotAmount, int totalWinners)
+		{
+			GameName = gameName;
+			DrawDate = drawDate;
+			WinningNumbers = winningNumbers;
+			BonusNumber = bonusNumber;
+			JackpotAmount = jackpotAmount;
+		}
+	}
+}
---
+++ LottoDataVisualiser/Services/LottoAPIservice.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LottoDataVisualiser.Services
+{
+	internal class LottoAPIservice
+	{
+	}
+}
