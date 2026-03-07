a73b420517a62a6fb0bc47f8496d9b62fa3fc42a
Frotrej <f.faszczowy@gmail.com>
2026-03-07 23:38:45 +0100

    fixes after agent took the wheel

--- Lotto.Tests/UnitTest1.cs
+++ Lotto.Tests/UnitTest1.cs
@@ -4,31 +4,6 @@ namespace Lotto.Tests
 {
 	public class UnitTest1
 	{
-		[Fact]
-		public void Test1()
-		{
-			var Lotto = new HttpClientService();
-
-			var result = Lotto.GetResults(2, 3);
-
-			Assert.NotNull(result);
-			Assert.Equal(6, result);
-
-		}
-
-		[Theory]
-		[InlineData(2, 3)]
-		[InlineData(4, 5)]
-		public void Test2(int a, int b)
-		{
-			var Lotto = new HttpClientService();
-			
-			var result = Lotto.GetResults(a, b);
-
-			Assert.NotNull(result);
-			Assert.Equal(a*b, result);
-
-		}
 
 	}
 }
