9f87a69063fff4cc11d80cc990f978f286864b4c
Frotrej <f.faszczowy@gmail.com>
2026-04-17 00:05:29 +0200

    new nuget implementation, research and new class

---
+++ LottoDataVisualiser/Services/Graphs/GraphTesting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using QuikGraph;
+
+namespace LottoApp.Services.Graphs
+{
+    class GraphTesting
+    {
+
+        AdjacencyGraph<string, Edge<string>> graph = new AdjacencyGraph<string, Edge<string>>();
+        DataSet dataSet = new DataSet();
+        DataSet
+	}
+}
