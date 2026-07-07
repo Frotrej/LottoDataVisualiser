51fa0b08b2759b07bbc535080a25e650d195c0e5
Frotrej <f.faszczowy@gmail.com>
2026-07-07 23:41:37 +0200

    QuikGraph went out the window. New nuget is ScottPlot and it will be used to draw graphs visualising api responses.

--- LottoDataVisualiser/Services/Graphs/GraphTesting.cs
+++ LottoDataVisualiser/Services/Graphs/GraphTesting.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Text;
-using QuikGraph;
 
 namespace LottoApp.Services.Graphs
 {
     class GraphTesting
     {
-        DataSet dataSet = new DataSet();
+       
 
 
 
